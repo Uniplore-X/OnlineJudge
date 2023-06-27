7b048319efc93d51de490cf7e1f8df2dcf1b83e1
ChenYaoGang <chenyg@uniplore.io>
2023-06-27 15:45:48 +0800

    wip: 初步加入登录labplore相关代码（OnlineJudge作为oauth2客户端）

--- labplore/urls/oj.py
+++ labplore/urls/oj.py
@@ -4,5 +4,5 @@ from ..views.oj import (RedirectWPLoginPageAPI, WPLoginAPI)
 
 urlpatterns = [
     url(r"^redirect_wplogin/?$", RedirectWPLoginPageAPI.as_view(), name="redirect_wordpress_login_page_api"),
-    url(r"^wplogin/?$", RedirectWPLoginPageAPI.as_view(), name="wordpress_login_api"),
+    url(r"^wplogin/?$", WPLoginAPI.as_view(), name="wordpress_login_api"),
 ]
